0c91cbb49f7e04b2d09d8aea57d06b739c7b25bc
Night-coder <akshit.pundir90@gmail.com>
2025-08-08 13:40:03 +0530

    Add SSL certificates and update project files

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const socket = io.connect('https://localhost:8181/')
+
+
+
+
 
 const localVideoEl = document.getElementById('local-video');
 const remoteVideo = document.getElementById('remote-video')
@@ -9,6 +14,53 @@ let remoteStream;
 let peerConnection
 
 
+let peerConfiguration = {
+    iceServers:[
+        {
+            urls:[
+              'stun:stun.l.google.com:19302',
+              'stun:stun1.l.google.com:19302'
+            ]
+        }
+    ]
+};
+
+
+// *************** GOOGLE STUN SERVER LIST**************
+
+//     { urls: "stun:stun.l.google.com:19302" },
+//     { urls: "stun:stun.l.google.com:5349" },
+//     { urls: "stun:stun1.l.google.com:3478" },
+//     { urls: "stun:stun1.l.google.com:5349" },
+//     { urls: "stun:stun2.l.google.com:19302" },
+//     { urls: "stun:stun2.l.google.com:5349" },
+//     { urls: "stun:stun3.l.google.com:3478" },
+//     { urls: "stun:stun3.l.google.com:5349" },
+//     { urls: "stun:stun4.l.google.com:19302" },
+//     { urls: "stun:stun4.l.google.com:5349" }
+
+
+
+const createPeerConnection = ( ) => {
+
+    return new Promise(async(resolve,reject) => {
+
+        peerConnection = await new RTCPeerConnection(peerConfiguration);
+
+        localStream.getTracks().forEach(track => {
+            peerConnection.addTrack(track,localStream);
+        })
+
+        peerConnection.addEventListener('icecandidate', ( e ) => {
+            console.log("ice candidate found ........!!!!");
+            console.log(e);
+        })
+        resolve();
+    });
+
+
+};
+
 
 const call = async( e ) => {
 
@@ -16,9 +68,36 @@ const call = async( e ) => {
         video:true
     });
     
-    localVideoEl.srcObject = stream
+    localVideoEl.srcObject = stream;
+    localStream = stream;
+
+    await createPeerConnection();
+
+    try {
+        
+        console.log("creating a offer");
+        const offer = await peerConnection.createOffer();
+        console.log(offer);
+        
+        peerConnection.setLocalDescription(offer);
+
+    } catch (error) {
+        console.log(error.message);
+    }
+
+
 }
 
 
 
 document.getElementById('call').addEventListener('click',call);
+
+
+
+
+
+
+
+
+
+
--- server.js
+++ server.js
@@ -1,19 +1,30 @@
+const https = require("https")
+const fs = require("fs");
 const express = require("express");
 
 const app = express();
-
-
-
+const socketIo = require("socket.io");
 app.use(express.static(__dirname));
 
+const key = fs.readFileSync('cert.key');
+const cert = fs.readFileSync('cert.crt');
+
+const expressServer = https.createServer({key,cert},app);
+const io = socketIo(expressServer);
 
 
-
-
-app.listen(8181,()=>{
+expressServer.listen(8181,()=>{
     console.log("server is live on port 8181");
 })
 
+
+io.on('connection' ,(socket) => {
+
+    console.log(`a user has connected to the server with id: ${socket.id}`);
+
+
+
+})
 
 
 
